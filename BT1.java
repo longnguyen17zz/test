6bd273149c94c9f68777bb1c0d57d14f00f88099
longnguyen17zz <longhq17@gmail.com>
2023-10-29 16:11:55 +0700

    Test

---
+++ BT1.java
@@ -0,0 +1,5 @@
+// tạo một packagés có tên là hình
+// trong packages tạo 1 lớp có têm hinhhoc với:
+// thuộc tính : chiều dài chiều rộng
+// phuơng thức : khởi tạo , nhập thông tin, xuất thông tin xác định xem hình nhập vào là hình vuông hay hcn
+// bên ngoài package tạo một lớp hình vuông thêm phuong thức chu vi và diện tích hv
